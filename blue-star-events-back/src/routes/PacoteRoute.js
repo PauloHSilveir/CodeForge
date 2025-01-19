40ad3b6d659604647fb26d1405bee207b927739e
Thiago Ferreira <134304261+Thiagoferreira13@users.noreply.github.com>
2025-01-19 18:28:24 -0300

    Update PacoteRoute.js
    corrigindo as rotas do pacote

--- blue-star-events-back/src/routes/PacoteRoute.js
+++ blue-star-events-back/src/routes/PacoteRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const PacoteController = require('../controllers/PacoteController'); 
-
+const pacoteController = require('../controllers/PacoteController'); 
 const authMiddleware = require('../middlewares/auth');
 
-//router.get('/pacote_pers', PacoteController.index);
-//router.post('/pacote_pers', authMiddleware, PacoteController.store);
+router.get('/:id',  pacoteController.getById);
+router.get('/', pacoteController.getAll);
+router.post('/cadastrar', pacoteController.create);
+router.delete('/delete/:id', pacoteController.delete);
+router.put('/update/:id', pacoteController.update);
 
 module.exports = router;
